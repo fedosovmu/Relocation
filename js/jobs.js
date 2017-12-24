53068fc97867d765b03d2e11eceac57af701a9fa
fedosovmu <fedosovmu@gmail.com>
2017-12-24 16:19:17 +0500

    Работающие теги

--- js/jobs.js
+++ js/jobs.js
@@ -19,7 +19,32 @@ function startTyping(input) {
     document.getElementById("main-content_search-button").style.display = 'block';
 }
 
-function quryEnter() {
+function quearyEnter() {
+    document.getElementById("main-content_search-button").style.display = 'none';
     var text = document.getElementById("main-content_search").value;
-    document.getElementById("search-head").innerHTML = text;
+    document.getElementById("main-content_search").value = '';
+    addTag(text);
+}
+
+var tags = [];
+function addTag(name) {
+    name = name[0].toUpperCase() + name.substr(1).toLowerCase();
+    tags = tags.filter(function (t) { return (t != name) });
+    tags.unshift(name);
+    printTags();
+}
+
+function deleteTag(input) {
+    var name = String(input.innerHTML);
+    name = name.replace(' <button class="tag_close">x</button>', '');
+    tags = tags.filter(function (t) { return (t != name) });
+    printTags();
+}
+
+function printTags() {
+    var line = '';
+    tags.forEach(function (item) {
+        line += '<div class="tag" onclick="deleteTag(this)">' + item + ' <button class="tag_close">x</button></div> ';
+    })
+    document.getElementById("main-content_tags").innerHTML = line;
 }
