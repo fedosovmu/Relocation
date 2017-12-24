edeca6756917cc216edd08936686d4a7799e9f5c
fedosovmu <fedosovmu@gmail.com>
2017-12-24 22:51:27 +0500

    Панелька со средними значениями при поиске

--- js/jobs.js
+++ js/jobs.js
@@ -30,6 +30,7 @@ function quearyEnter() {
 
 var tags = [];
 function addTag(name) {
+    name = name.trim();
     name = name[0].toUpperCase() + name.substr(1).toLowerCase();
     tags = tags.filter(function (t) { return (t != name) });
     tags.unshift(name);
@@ -58,7 +59,7 @@ function getResults() {
     if (tags.length == 0) return;
 
     var vacancies = vacanciesData;
-    
+
     if (currentCountryId != "")
         vacancies = vacancies.filter(function (t) { return t.id.toUpperCase() == currentCountryId; });
 
@@ -70,24 +71,58 @@ function getResults() {
             return (a.salary < b.salary) ? 1:-1;
         }));
 
+    var sum = 0;
+    vacancies.forEach(function (t) { sum += t.salary });
+    var avg = sum / vacancies.length;
+
+
+    var result =
+        '<div class="extend-content_panel">' +
+        '    <div class="result_caption">Количество предложений</div>' +
+        '    <div>' + vacancies.length + '</div><br>' +
+        '    <div class="result_caption">Средняя зарплата</div>' +
+        '    <div>' + avg + '</div><br>' +
+        '    <div class="result_caption">Распределение по странам</div>' +
+        '    <div>';
+
+    var codes = [];
+    vacancies.forEach(function (t) { codes.push(t.id.toUpperCase())});
+    codes = codes.filter(function(item) {
+        return codes.hasOwnProperty(item) ? false : (codes[item] = true);
+    });
+    codes.sort();
+    codes.forEach(function (id) {
+        var count = 0;
+        vacancies.forEach( function (t) { if (t.id.toUpperCase() == id) count += 1; });
+        result += '<b>' + id + '</b> - ' + count + '<br>';
+    })
+
+    result += '</div>'
+              '</div>';
+
+    document.getElementById("extend-content").innerHTML = result;
+
+
     vacancies = vacancies.filter(function (t, i) { return i < 10; });
 
-
     vacancies.forEach(function (t) {
+        var href =  t.href;
+        if (href == undefined) href = '';
+
         document.getElementById("main-content_results").innerHTML +=
-        '<div class="result">' +
-        '    <div><a class="result_caption" href="' + t.href + '#">' + t.name + '</a></div>\n' +
-        '    <div class="result-info-block">\n' +
-        '        <div class="result_info1" style="max-width: 50%;">\n' +
-        '            ' + t.id + ', ' + t.city +
-        '        </div>\n' +
-        '        <div class="result_info2" style="max-width: 50%;">\n' +
-        '            '+ t.company + ', ' + t.salary +
-        '        </div>\n' +
-        '    </div>' +
-        '    <div class="result_text">' +
-        '        ' + t.desc +
-        '    </div>' +
-        '</div>';
+            '<div class="result">' +
+            '    <div><a class="result_caption" target="_blank" href="' + t.href + '#">' + t.name + '</a></div>\n' +
+            '    <div class="result-info-block">\n' +
+            '        <div class="result_info1" style="max-width: 50%;">\n' +
+            '            ' + t.id + ', ' + t.city +
+            '        </div>\n' +
+            '        <div class="result_info2" style="max-width: 50%;">\n' +
+            '            '+ t.company + ', ' + t.salary +
+            '        </div>\n' +
+            '    </div>' +
+            '    <div class="result_text">' +
+            '        ' + t.desc +
+            '    </div>' +
+            '</div>';
     });
 }
